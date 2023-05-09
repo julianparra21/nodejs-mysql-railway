fa629a828d2cbc3140ab6d27c99984935a7e05a3
cohnante <gian-5634@hotmail.com>
2023-05-09 11:19:34 -0500

    name update

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ res.json(result[0])
 
 
 app.get('/create',async(req,res)=>{
-   const result= await pool.query(`INSERT into users(name) VALUES("Jonh")`)
+   const result= await pool.query(`INSERT into users(name) VALUES("cuchillo")`)
 
    res.json(result)
 })
